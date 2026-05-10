134480771ccb8675b11e0ae4a5934467386d95db
benjaminoyarzun <benjaminoyarzuninfo@gmail.com>
2026-05-10 03:21:25 -0300

    10/05 ej4

--- Gestor.java
+++ Gestor.java
@@ -1,47 +1,101 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gestor {
     public static void main(String[] args) {
 
-        
-        
-        
-        
     }
-    
+
 }
- class Tarea {
+
+class Tarea {
     String nombre;
     String descripcion;
-    String estado;
+    String estado="pendiente";
     String fechaLimite;
-    
 
+    void actualizarEstado(Scanner sc) {
 
-    
+    String nuevoEstado;
+
+    do {
+
+        System.out.println("""
+        Estados validos:
+        - Pendiente
+        - En Progreso
+        - Completada
+        """);
+
+        System.out.print(
+            "Ingrese nuevo estado: "
+        );
+
+        nuevoEstado = sc.nextLine();
+
+    } while (
+        !nuevoEstado.equalsIgnoreCase("Pendiente")
+        &&
+        !nuevoEstado.equalsIgnoreCase("En Progreso")
+        &&
+        !nuevoEstado.equalsIgnoreCase("Completada")
+    );
+
+    estado = nuevoEstado;
+
+    System.out.println(
+        "Estado actualizado."
+    );
 }
- class miembroEquipo {
+}
+
+class miembroEquipo {
     String nombre;
     String rol;
-    ArrayList <Tarea> tareasAsignadas = new ArrayList<>();
- 
-    
+    ArrayList<Tarea> tareasAsignadas = new ArrayList<>();
+
 }
- class Proyecto{
+
+class Proyecto {
     String nombre;
     String fechaInicio;
     String fechaFinEstimada;
     ArrayList<Tarea> tareasProyecto = new ArrayList<>();
     ArrayList<miembroEquipo> miembrosProyecto = new ArrayList<>();
 
-
-
-    void checkearTareas(){
+    void checkearTareas() {
         for (Tarea tarea : tareasProyecto) {
-            System.out.printf("d% - d%",tarea.nombre,tarea.estado);
+            System.out.printf("s% - s%", tarea.nombre, tarea.estado);
 
-            
         }
     }
 
+    void agregarTarea(Tarea tarea) {
+        tareasProyecto.add(tarea);
+    }
+
+
+    void asignarTarea(Tarea tarea) {
+        int i = 0;
+
+        while (i < miembrosProyecto.size()) {
+
+            miembroEquipo miembro = miembrosProyecto.get(i);
+
+            if (miembro.tareasAsignadas.size() < 3) {
+
+                miembro.tareasAsignadas.add(tarea);
+
+                System.out.printf( "Tarea asignada a %s",miembro.nombre);
+
+                return;
+            }
+
+            i++;
+        }
+
+        System.out.println(
+                "No hay miembros disponibles.");
+
+    }
 }
